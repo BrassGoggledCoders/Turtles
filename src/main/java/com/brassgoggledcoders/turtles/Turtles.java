95a7e100ec230fb7aaa06fc4a2cf19b5fe209bd5
Skylar <skylar.som@gmail.com>
2022-09-25 21:08:19 -0400

    Remove more stuff

--- src/main/java/com/brassgoggledcoders/turtles/Turtles.java
+++ src/main/java/com/brassgoggledcoders/turtles/Turtles.java
@@ -1,9 +1,13 @@
 package com.brassgoggledcoders.turtles;
 
 import com.brassgoggledcoders.turtles.farmersdelight.FarmersDelightTurtles;
+import com.brassgoggledcoders.turtles.pneumaticcraft.PneumaticCraftTurtles;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 @Mod(Turtles.ID)
@@ -11,12 +15,12 @@ public class Turtles {
     public static final String ID = "turtles";
 
     public Turtles() {
-        tryActivateMod("farmersdelight", () -> FarmersDelightTurtles::new);
+        tryActivateMod("pneumaticcraft", () -> PneumaticCraftTurtles::setup);
     }
 
-    public void tryActivateMod(String modId, Supplier<Supplier<?>> contentSupplier) {
+    public void tryActivateMod(String modId, Supplier<Consumer<IEventBus>> contentSupplier) {
         if (ModList.get().isLoaded(modId)) {
-            contentSupplier.get().get();
+            contentSupplier.get().accept(FMLJavaModLoadingContext.get().getModEventBus());
         }
     }
 }
